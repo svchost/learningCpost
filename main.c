958d6a0b67f1b2aa9f7d0a2d88aaa2143546d809
svchost <femboys@null.net>
2022-03-26 17:31:37 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int men;
+
+    printf("how much do you love femboys 1 - 100 \n");
+    scanf_s("%d", &men);
+    if (men > 0)
+        while (men > 0) {
+            printf("kysss \n");
+            men--;
+        };
+        printf("please\n\n");
+
+
+    return 0;
+};
