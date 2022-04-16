01cb59b6096b7a87a0b596eba15b13ba45e92e48
draco <femboys@null.net>
2022-04-16 08:37:42 -0700

    Create getmoneyfuckbitches.c
    im bored

---
+++ getmoneyfuckbitches.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+	int bitches;
+	printf("get money fuck bitches? 1 for yes 2 for no\n");
+	scanf_s("%d", &bitches);
+	if (bitches > 1)
+		printf("you get no bitches and no money nigga \n");
+		
+	else
+		printf("fuck bitches get money \n");
+	
+
+	return 0;
+
+}
